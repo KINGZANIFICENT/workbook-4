5278187a4b0caca1b1fdbc86d22be7bf962ddb75
KINGZANIFICENT <chefangel.vargas23@gmail.com>
2025-05-06 10:53:28 -0400

    added hotel ops

---
+++ HotelOperations/src/main/java/pluralsight/Employee.java
@@ -0,0 +1,89 @@
+package pluralsight;
+
+public class Employee {
+
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private float hoursWorked;
+
+    public Employee(int employeeId, String name, String department, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public float getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(float hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getTotalPay(){
+        return (getRegularHours() * payRate) + (getOvertimeHours() * payRate * 1.5);
+    }
+
+    public float getRegularHours(){
+        if(hoursWorked > 40){
+            return 40;
+        }
+        else{
+            return hoursWorked;
+        }
+    }
+
+    public float getOvertimeHours(){
+//        if(hoursWorked > 40){
+//            return hoursWorked - 40;
+//        }
+//        else{
+//            return 0;
+//        }
+        return (hoursWorked > 40) ? hoursWorked - 40 : 0;
+    }
+
+    private double punchInTime;
+
+    public void punchIn(double time){
+        this.punchInTime = time;
+    }
+
+    public void punchOut(double punchOutTime){
+
+        this.hoursWorked +=  (punchOutTime - this.punchInTime);
+
+    }
+
+}
---
+++ HotelOperations/src/main/java/pluralsight/Room.java
@@ -0,0 +1,59 @@
+package pluralsight;
+
+public class Room {
+
+    private int numberOfBeds;
+    private double price;
+    private boolean occupied;
+    private boolean dirty;
+
+    public Room(int numberOfBeds, double price) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.occupied = false;
+        this.dirty = false;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+//    public void setOccupied(boolean occupied) {
+//        this.occupied = occupied;
+//    }
+
+    public boolean isDirty() {
+        return dirty;
+    }
+
+//    public void setDirty(boolean dirty) {
+//        this.dirty = dirty;
+//    }
+
+    public boolean isAvailable() {
+        return (!occupied) && (!dirty);
+    }
+
+    public void checkIn(){
+        this.occupied = true;
+        this.dirty = true;
+    }
+
+    public void checkOut(){
+        this.occupied = false;
+    }
+
+    public void cleanRoom(){
+        this.dirty = false;
+    }
+
+
+}
